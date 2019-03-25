c076cfd36dee9c0a0ed1a11c1fdea45054385a7e
haneefpervez1 <haneef.pervez@gmail.com>
2019-03-25 17:45:07 -0400

    print files

--- fileCompressor.c
+++ fileCompressor.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
 
 int main (int argc, char** argv) {
-	printf("fileCompressor\n");
+	/*
+		Prints out files in current directory
+	*/
+	struct dirent *de;
+	
+	DIR *dr = opendir(".");
+	
+	if (dr == NULL) {
+		printf("error\n");
+	}
+	while ((de = readdir(dr)) != NULL) {
+		printf("%s\n", de->d_name);
+	}
+	closedir(dr);
 	return 0;
 }
+
